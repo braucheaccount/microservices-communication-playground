a8870ec1c802fb6020300fc63299b3deab3aee50
smj187 <braucheaccount@protonmail.com>
2022-03-30 15:14:49 +0200

    fix naming

---
+++ UserService/Contracts/v1/Requests/CreateUserRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Contracts.v1.Requests
+{
+    public class CreateUserRequest
+    {
+        public string Username { get; set; }
+
+    }
+}
---
+++ UserService/Repositories/UserRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Data;
+using UserService.Models;
+
+namespace UserService.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserContext _context;
+
+        public UserRepository(UserContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> CreateUserAsync(User newUser)
+        {
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+
+            return newUser;
+        }
+
+        public async Task DeleteUserAsync(Guid id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (user == null)
+            {
+                throw new ArgumentNullException($"product '{id}' is null");
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User> FindUserByIdAsync(Guid id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (user == null)
+            {
+                throw new ArgumentNullException($"product '{id}' is null");
+            }
+
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> ListUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
